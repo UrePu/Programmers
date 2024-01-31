7b792d5f7667eb8e83af5a49e1d081b55658cd93
UrePu <85550543+UrePu@users.noreply.github.com>
2024-01-31 22:56:02 +0900

    Num 16

--- programmer.js
+++ programmer.js
@@ -483,18 +483,45 @@
 
 
 //15번
-console.log(solution(6))
-function solution(num) {
-    var answer = 0;
-    if(num === 1){
-    }else{
-        do{
-            (num%2 === 0) ? num = num / 2 : num = (num * 3) + 1
-            answer++
-        }while(num > 1 && answer < 500)
-    }
-    if(answer === 500) answer = -1
-    return answer;
-}
+// console.log(solution(6))
+// function solution(num) {
+//     var answer = 0;
+//     if(num === 1){
+//     }else{
+//         do{
+//             (num%2 === 0) ? num = num / 2 : num = (num * 3) + 1
+//             answer++
+//         }while(num > 1 && answer < 500)
+//     }
+//     if(answer === 500) answer = -1
+//     return answer;
+// }
 
 //좀 깔끔하게 된거같다
+
+//16 번
+
+// 문제 설명
+
+// String형 배열 seoul의 element중 "Kim"의 위치 x를 찾아, "김서방은 x에 있다"는 String을 반환하는 함수, solution을 완성하세요. seoul에 "Kim"은 오직 한 번만 나타나며 잘못된 값이 입력되는 경우는 없습니다.
+// 제한 사항
+
+//     seoul은 길이 1 이상, 1000 이하인 배열입니다.
+//     seoul의 원소는 길이 1 이상, 20 이하인 문자열입니다.
+//     "Kim"은 반드시 seoul 안에 포함되어 있습니다.
+
+// 입출력 예
+// seoul 	return
+// ["Jane", "Kim"] 	"김서방은 1에 있다"
+
+// function solution(seoul) {
+//     var answer = "";
+//     for(var i = 0; i < seoul.length;i++){
+//         if(seoul[i] == "Kim")
+//             answer = "김서방은 " + i + "에 있다";
+//             break;
+//     }
+//     return answer;
+// }
+
+//seoul 배열의 모든 인덱스를 
