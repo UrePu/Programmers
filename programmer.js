37474bb5fe390ee9f6f967ab01a1da5fa3d1ab1f
Your Name <darkin31313@gmail.com>
2024-03-18 16:34:31 +0900

    num 27

--- programmer.js
+++ programmer.js
@@ -925,3 +925,34 @@
 //     }
 //     return answer;
 // }
+
+//27번
+
+// 문제 설명
+// 행렬의 덧셈은 행과 열의 크기가 같은 두 행렬의 같은 행, 같은 열의 값을 서로 더한 결과가 됩니다. 2개의 행렬 arr1과 arr2를 입력받아, 행렬 덧셈의 결과를 반환하는 함수, solution을 완성해주세요.
+
+// 제한 조건
+// 행렬 arr1, arr2의 행과 열의 길이는 500을 넘지 않습니다.
+// 입출력 예
+// arr1	arr2	return
+// [[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
+// [[1],[2]]	[[3],[4]]
+console.table(solution([[1,2] , [2,3]] , [[3,4],[5,6]]))
+console.table(solution([[1,2]] , [[3,4]]))
+// console.log([[1,2] , [2,3]] , [[3,4],[5,6]])
+function solution(arr1, arr2) {
+    var answer = [[]];
+    
+    for(var i = 0; i < arr1.length; i++){
+        var sum = [];
+
+        for(var j = 0 ; j < arr2[0].length; j++){
+            sum[j] = arr1[i][j] + arr2[i][j]
+            // console.log(sum);
+        }
+        answer[i] = sum
+    }
+    return answer;
+}
+
+[[[4],[6]],[[7],[9]]]
