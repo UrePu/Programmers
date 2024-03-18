f7d5965363d3098663b50bed0cef87381f34363c
Your Name <darkin31313@gmail.com>
2024-03-18 16:51:56 +0900

    28번

--- programmer.js
+++ programmer.js
@@ -937,22 +937,52 @@
 // arr1	arr2	return
 // [[1,2],[2,3]]	[[3,4],[5,6]]	[[4,6],[7,9]]
 // [[1],[2]]	[[3],[4]]
-console.table(solution([[1,2] , [2,3]] , [[3,4],[5,6]]))
-console.table(solution([[1,2]] , [[3,4]]))
-// console.log([[1,2] , [2,3]] , [[3,4],[5,6]])
-function solution(arr1, arr2) {
-    var answer = [[]];
+// console.table(solution([[1,2] , [2,3]] , [[3,4],[5,6]]))
+// console.table(solution([[1,2]] , [[3,4]]))
+// // console.log([[1,2] , [2,3]] , [[3,4],[5,6]])
+// function solution(arr1, arr2) {
+//     var answer = [[]];
     
-    for(var i = 0; i < arr1.length; i++){
-        var sum = [];
+//     for(var i = 0; i < arr1.length; i++){
+//         var sum = [];
 
-        for(var j = 0 ; j < arr2[0].length; j++){
-            sum[j] = arr1[i][j] + arr2[i][j]
-            // console.log(sum);
-        }
-        answer[i] = sum
-    }
-    return answer;
-}
+//         for(var j = 0 ; j < arr2[0].length; j++){
+//             sum[j] = arr1[i][j] + arr2[i][j]
+//             // console.log(sum);
+//         }
+//         answer[i] = sum
+//     }
+//     return answer;
+// }
 
-[[[4],[6]],[[7],[9]]]
+
+//num 28
+
+// 문제 설명
+// 이 문제에는 표준 입력으로 두 개의 정수 n과 m이 주어집니다.
+// 별(*) 문자를 이용해 가로의 길이가 n, 세로의 길이가 m인 직사각형 형태를 출력해보세요.
+
+// 제한 조건
+// n과 m은 각각 1000 이하인 자연수입니다.
+// 예시
+// 입력
+
+// 5 3
+// 출력
+
+// *****
+// *****
+// *****
+
+// process.stdin.setEncoding('utf8');
+// process.stdin.on('data', data => {
+//     const n = data.split(" ");
+//     const a = Number(n[0]), b = Number(n[1]);
+//     for(var i = 0; i < b; i ++){
+//         var sum = ""
+//         for(var j = 0; j < a; j++){
+//             sum += "*"
+//         }
+//         console.log(sum)
+//     }
+// });
