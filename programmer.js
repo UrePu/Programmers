4adf3d6cdff8e5e8f035ef51185659a72ac7db72
Your Name <darkin31313@gmail.com>
2024-03-19 17:59:12 +0900

    asdf

--- programmer.js
+++ programmer.js
@@ -986,3 +986,63 @@
 //         console.log(sum)
 //     }
 // });
+
+//NUM 29
+
+// 문제 설명
+// 두 수를 입력받아 두 수의 최대공약수와 최소공배수를 반환하는 함수, solution을 완성해 보세요.
+// 배열의 맨 앞에 최대공약수, 그다음 최소공배수를 넣어 반환하면 됩니다.
+// 예를 들어 두 수 3, 12의 최대공약수는 3, 최소공배수는 12이므로 solution(3, 12)는 [3, 12]를 반환해야 합니다.
+
+// 제한 사항
+// 두 수는 1이상 1000000이하의 자연수입니다.
+// 입출력 예
+// n	m	return
+// 3	12	[3, 12]
+// 2	5	[1, 10]
+// 입출력 예 설명
+// 입출력 예 #1
+// 위의 설명과 같습니다.
+
+// 입출력 예 #2
+// 자연수 2와 5의 최대공약수는 1, 최소공배수는 10이므로 [1, 10]을 리턴해야 합니다.
+
+console.log(solution(2,10));
+
+function measure(a) {
+    var ary = [];
+        for(var i = 1 ; i <= a; i++){
+            if(a % i == 0){
+                ary.push(i)
+            }
+        }
+    return ary
+}
+
+function measMax(a, b) {
+    var max = 0;
+    for(var i =0; i < a.length; i++){
+        for(var j=0; j < b.length; j++){
+            if(a[i] == b[j]){
+                if(max < b[j]){
+                    max = b[j];
+                }
+            }
+        }
+    }
+    return max;
+}
+
+function measMin(a, b) {
+    
+}
+
+function solution(n, m) {
+    var answer = [];
+    var measn = measure(n);
+    var measm = measure(m);
+    answer = [measMax(measn, measm) , measMin(measn, measm)];
+    return answer;
+}
+
+
