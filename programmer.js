f3aafc7570866518ffd1b7bba1dac4caa78ed51b
UrePu <darkin31313@gmail.com>
2024-11-02 11:32:25 +0900

    문제 36

--- programmer.js
+++ programmer.js
@@ -1287,3 +1287,40 @@
 // }
 
 //문제의 예외나 테스트 케이스를 잘 생각해보기.
+
+// //문제 36
+
+// 문제 설명
+// 자연수 n이 매개변수로 주어집니다. n을 3진법 상에서 앞뒤로 뒤집은 후, 이를 다시 10진법으로 표현한 수를 return 하도록 solution 함수를 완성해주세요.
+
+// 제한사항
+// n은 1 이상 100,000,000 이하인 자연수입니다.
+
+//내풀이
+// solution(45)
+// function solution(n) {
+//     var answer = 0;
+//     var arrN = []
+//     while(true){
+//         arrN.push(n % 3)
+//         if(n/3 < 1) break
+//         n = Math.floor(n / 3)
+//     }
+//     // console.log(arrN);
+    
+//     for(let i = 0; i < arrN.length ; i ++){
+//         answer += arrN[i] * 3**(arrN.length -1 - i)
+//         console.log(3**(arrN.length - i));
+        
+//     }
+//     // console.log(answer);
+    
+
+//     return answer;
+// }
+
+//다른사람풀이
+
+// const solution = (n) => {
+//     return parseInt([...n.toString(3)].reverse().join(""), 3);
+// }
