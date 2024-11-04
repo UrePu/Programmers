9cc2d5ef20ad940784ae16a93de225b8c218fc61
UrePu <darkin31313@gmail.com>
2024-11-04 21:48:58 +0900

    문제 37

--- programmer.js
+++ programmer.js
@@ -1324,3 +1324,45 @@
 // const solution = (n) => {
 //     return parseInt([...n.toString(3)].reverse().join(""), 3);
 // }
+
+//문제 37
+
+// 문제 설명
+// 한국중학교에 다니는 학생들은 각자 정수 번호를 갖고 있습니다. 이 학교 학생 3명의 정수 번호를 더했을 때 0이 되면 3명의 학생은 삼총사라고 합니다. 예를 들어, 5명의 학생이 있고, 각각의 정수 번호가 순서대로 -2, 3, 0, 2, -5일 때, 첫 번째, 세 번째, 네 번째 학생의 정수 번호를 더하면 0이므로 세 학생은 삼총사입니다. 또한, 두 번째, 네 번째, 다섯 번째 학생의 정수 번호를 더해도 0이므로 세 학생도 삼총사입니다. 따라서 이 경우 한국중학교에서는 두 가지 방법으로 삼총사를 만들 수 있습니다.
+
+// 한국중학교 학생들의 번호를 나타내는 정수 배열 number가 매개변수로 주어질 때, 학생들 중 삼총사를 만들 수 있는 방법의 수를 return 하도록 solution 함수를 완성하세요.
+
+// 제한사항
+// 3 ≤ number의 길이 ≤ 13
+// -1,000 ≤ number의 각 원소 ≤ 1,000
+// 서로 다른 학생의 정수 번호가 같을 수 있습니다.
+
+solution([-2, 3, 0, 2, -5])
+function solution(number) {
+    var answer = 0;
+    
+    for(let i = 0 ; i < number.length ; i++){
+        for(let j = 0; j < number.length; j++){
+            if(i == j){
+                // console.log(number[i]+ " "+ number[j]+ " "+ i+ " "+ " " + j + " SUM + NUMBER");
+            }else{
+                var sum = number[i] + number[j]
+                // console.log(sum + "SUM 값");
+                
+                for(let x = 0; x < number.length; x ++){
+                    if(x == i || x == j){
+                        console.log(i + " " + j + " " + x + " ijx값");
+                        
+                        // console.log(number[i] + " " + number[j] + " " + number[x] + " 예외처리");
+                    } else if(sum + number[x] == 0){
+                        console.log(number[i] + " " + number[j] + " " + number[x] + " 답인 NUMI NUMJ NUMX");
+                        answer++
+                    }
+                }
+            }
+        }
+    }
+    console.log(answer); 
+    
+    return answer;
+}
