0607b517582a2d4d68abf0814fcb6b26ab8001b0
UrePu <darkin31313@gmail.com>
2024-11-13 14:35:40 +0900

    41m

--- programmer.js
+++ programmer.js
@@ -1549,3 +1549,70 @@
 
 
 //문제 41
+
+// 네오와 프로도가 숫자놀이를 하고 있습니다. 네오가 프로도에게 숫자를 건넬 때 일부 자릿수를 영단어로 바꾼 카드를 건네주면 프로도는 원래 숫자를 찾는 게임입니다.
+
+// 다음은 숫자의 일부 자릿수를 영단어로 바꾸는 예시입니다.
+
+// 1478 → "one4seveneight"
+// 234567 → "23four5six7"
+// 10203 → "1zerotwozero3"
+// 이렇게 숫자의 일부 자릿수가 영단어로 바뀌어졌거나, 혹은 바뀌지 않고 그대로인 문자열 s가 매개변수로 주어집니다. s가 의미하는 원래 숫자를 return 하도록 solution 함수를 완성해주세요.
+
+// 참고로 각 숫자에 대응되는 영단어는 다음 표와 같습니다.
+
+// 숫자	영단어
+// 0	zero
+// 1	one
+// 2	two
+// 3	three
+// 4	four
+// 5	five
+// 6	six
+// 7	seven
+// 8	eight
+// 9	nine
+solution("1zerotwozero3")
+// function solution(s) {
+//     var arrS = s.split("")
+//     var arrW = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+//     var word = ''
+//     var answer = '';
+//     for(let i = 0; i < arrS.length; i++){
+//         if(arrS[i] * 1 >= 0){
+//             answer += arrS[i] 
+//             // console.log(i);
+                       
+//         }else {
+//             word += arrS[i]
+//             if(word.length > 2){
+//                 console.log(word);
+//                 for(let j = 0; j < arrW.length; j++){
+//                     if(word == arrW[j]){
+//                         answer += j
+//                         word = ''
+//                         break;
+//                     }
+//                 }
+//             }
+            
+//         }
+//     }
+
+//     answer = Number(answer)
+//     // console.log(answer);
+    
+//     return answer;
+// }
+
+// function solution(s) {
+//     let numbers = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"];
+//     var answer = s;
+
+//     for(let i=0; i< numbers.length; i++) {
+//         let arr = answer.split(numbers[i]);
+//         answer = arr.join(i);
+//     }
+
+//     return Number(answer);
+// }
