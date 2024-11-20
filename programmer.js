82a409f659245d3926f3630c2b2a4662344af86f
UrePu <darkin31313@gmail.com>
2024-11-20 17:17:31 +0900

    50m

--- programmer.js
+++ programmer.js
@@ -1948,3 +1948,12 @@
 // nums의 길이(N)는 1 이상 10,000 이하의 자연수이며, 항상 짝수로 주어집니다.
 // 폰켓몬의 종류 번호는 1 이상 200,000 이하의 자연수로 나타냅니다.
 // 가장 많은 종류의 폰켓몬을 선택하는 방법이 여러 가지인 경우에도, 선택할 수 있는 폰켓몬 종류 개수의 최댓값 하나만 return 하면 됩니다.
+// solution([3, 3, 3, 2, 2, 4]);
+// function solution(nums) {
+//   let i = nums.length / 2;
+//   let arr = [...new Set(nums)];
+//   answer = arr.length > i ? i : arr.length;
+//   //   console.log(answer);
+
+//   return answer;
+// }
