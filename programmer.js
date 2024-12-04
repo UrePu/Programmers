f75e16c684dfa7b47a4d82f43d85785c469a935d
UrePu <darkin31313@gmail.com>
2024-12-04 17:52:59 +0900

    a

--- programmer.js
+++ programmer.js
@@ -2223,3 +2223,29 @@
 
 //   return answer;
 // }
+
+//gpt해결
+// function solution(nums) {
+//     let answer = 0;
+
+//     // Helper function to check if a number is a prime
+//     function isPrime(num) {
+//       if (num < 2) return false;
+//       for (let i = 2; i * i <= num; i++) {
+//         if (num % i === 0) return false;
+//       }
+//       return true;
+//     }
+
+//     // Iterate through all combinations of 3 numbers
+//     for (let i = 0; i < nums.length - 2; i++) {
+//       for (let j = i + 1; j < nums.length - 1; j++) {
+//         for (let k = j + 1; k < nums.length; k++) {
+//           const sum = nums[i] + nums[j] + nums[k];
+//           if (isPrime(sum)) answer++;
+//         }
+//       }
+//     }
+
+//     return answer;
+//   }
